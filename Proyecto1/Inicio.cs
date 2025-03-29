0de75414ff73e0cacde63e37941f14790e831954
Lorenzo López <lorenzolpz784@gmail.com>
2025-03-29 15:43:44 -0600

    feat(index): add chart

--- Proyecto1/Inicio.cs
+++ Proyecto1/Inicio.cs
@@ -1,6 +1,8 @@
 ﻿using Proyecto1.Forms.Distribucion;
 using Proyecto1.Forms.Productos;
 using Proyecto1.Forms.Proveedores;
+using Proyecto1.Modelos;
+using Proyecto1.Servicios;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -10,6 +12,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Proyecto1
 {
@@ -65,6 +68,33 @@ namespace Proyecto1
 
             panelContent.Controls.Add(frm);
             frm.Show();
+        }
+
+        private void panelContent_Paint(object sender, PaintEventArgs e)
+        {
+            DataTable dt = ReporteService.ObtenerCantidadProductosPorProveedor();
+
+            chart1.Series.Clear();
+            chart1.ChartAreas.Clear();
+            chart1.ChartAreas.Add(new ChartArea("chartArea"));
+            chart1.Series.Add("CantidadProductos");
+            chart1.Series["CantidadProductos"].XValueMember = "Proveedor";
+            chart1.Series["CantidadProductos"].YValueMembers = "TotalProductos";
+            chart1.Series["CantidadProductos"].ChartType = SeriesChartType.Bar;
+
+            chart1.DataSource = dt;
+            chart1.DataBind();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (numericUpDown1.Value > 0)
+            {
+                dataGridView1.DataSource = ReporteService.ObtenerProductosStockBajo((int)numericUpDown1.Value);
+                return;
+            }
+            MessageBox.Show("Ingrese una cantidad de stock", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
         }
     }
 }
---
+++ Proyecto1/Servicios/ReporteService.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto1.Servicios
+{
+    class ReporteService
+    {
+        public static DataTable ObtenerProductosStockBajo(int stockLimite)
+        {
+            using (SqlConnection conn = new Conexion().AbrirConexion())
+            {
+                string query = "SELECT nombre, stock FROM productos WHERE stock < @StockLimite";
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                da.SelectCommand.Parameters.AddWithValue("@StockLimite", stockLimite);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                return dt;
+            }
+        }
+        public static DataTable ObtenerProductosPorFecha(DateTime fechaInicio, DateTime fechaFin)
+        {
+            using (SqlConnection conn = new Conexion().AbrirConexion())
+            {
+                string query = "SELECT nombre, fecha_ingreso FROM productos WHERE fecha_ingreso BETWEEN @FechaInicio AND @FechaFin";
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                da.SelectCommand.Parameters.AddWithValue("@FechaInicio", fechaInicio);
+                da.SelectCommand.Parameters.AddWithValue("@FechaFin", fechaFin);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                return dt;
+            }
+        }
+
+        public static DataTable ObtenerProveedoresYProductos()
+        {
+            using (SqlConnection conn = new Conexion().AbrirConexion())
+            {
+                string query = @"
+                    SELECT p.nombre AS Proveedor, pr.nombre AS Producto, pp.cantidad
+                    FROM proveedores p
+                    INNER JOIN producto_proveedor pp ON p.id = pp.id_proveedor
+                    INNER JOIN productos pr ON pp.id_producto = pr.id";
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                return dt;
+            }
+        }
+
+        public static DataTable ObtenerProveedoresYProductosLeftJoin()
+        {
+            using (SqlConnection conn = new Conexion().AbrirConexion())
+            {
+                string query = @"
+                    SELECT p.nombre AS Proveedor, pr.nombre AS Producto, pp.cantidad
+                    FROM proveedores p
+                    LEFT JOIN producto_proveedor pp ON p.id = pp.id_proveedor
+                    LEFT JOIN productos pr ON pp.id_producto = pr.id";
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                return dt;
+            }
+        }
+
+        public static DataTable ObtenerProveedoresYProductosRightJoin()
+        {
+            using (SqlConnection conn = new Conexion().AbrirConexion())
+            {
+                string query = @"
+                    SELECT p.nombre AS Proveedor, pr.nombre AS Producto, pp.cantidad
+                    FROM proveedores p
+                    RIGHT JOIN producto_proveedor pp ON p.id = pp.id_proveedor
+                    RIGHT JOIN productos pr ON pp.id_producto = pr.id";
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                return dt;
+            }
+        }
+        public static DataTable ObtenerCantidadProductosPorProveedor()
+        {
+            using (SqlConnection conn = new Conexion().AbrirConexion())
+            {
+                string query = @"
+                    SELECT p.nombre AS Proveedor, SUM(pp.cantidad) AS TotalProductos
+                    FROM proveedores p
+                    INNER JOIN producto_proveedor pp ON p.id = pp.id_proveedor
+                    GROUP BY p.nombre";
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                return dt;
+            }
+        }
+
+
+    }
+}
