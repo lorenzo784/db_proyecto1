8f5be3f48b95c54c0e37a468148301c8a4de8bac
Lorenzo López <lorenzolpz784@gmail.com>
2025-03-29 13:42:06 -0600

    feat(suppliers): add suppliers list

---
+++ Proyecto1/Forms/Proveedores/ListarProveedores.cs
@@ -0,0 +1,28 @@
+﻿using Proyecto1.Servicios;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto1.Forms.Proveedores
+{
+    public partial class ListarProveedores : Form
+    {
+        private Inicio inicio;
+        public ListarProveedores(Inicio inicio)
+        {
+            this.inicio = inicio;
+            InitializeComponent();
+        }
+
+        private void ListarProveedores_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = ProveedorService.ObtenerProveedores();
+        }
+    }
+}
